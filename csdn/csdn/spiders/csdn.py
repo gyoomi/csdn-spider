ecba3a21c20412d5a5e14e3aad774c148f7b54a3
leon <gyoomi0709@foxmail.com>
2020-05-02 20:22:27 +0800

    Initial commit

---
+++ csdn/csdn/spiders/csdn.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CsdnSpider(scrapy.Spider):
+    name = 'csdn'
+    allowed_domains = ['csdn.net']
+    start_urls = ['http://csdn.net/']
+
+    def parse(self, response):
+        print("ok!!!!!!!!!!")
+        pass
